38834af7e749fe50c9c8e833002b84b22afb002d
ArTuRaZ7 <49605245+ArTuRaZ7@users.noreply.github.com>
2025-06-06 11:46:08 +0500

    Update train.py

--- train.py
+++ train.py
@@ -7,63 +7,53 @@ import torch
 import torch.nn as nn
 from torch import optim
 from tensorboardX import SummaryWriter
+from torch.cuda.amp import autocast, GradScaler  # Добавлено для AMP [[9]]
 
 from model import UNet, make_dataloaders, eval_net_loader, make_checkpoint_dir
 from lib import plot_net_predictions
 
 
-def train_epoch(epoch,train_loader,criterion,optimizer,batch_size,scheduler):
-    
+def train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler):
     net.train()
     epoch_loss = 0
     
     for i, sample_batch in enumerate(train_loader):
-
         imgs = sample_batch['image']
         true_masks = sample_batch['mask']
 
         imgs = imgs.to(device)
-        true_masks = true_masks.to(device)
+        true_masks = true_masks.long().to(device)  # Преобразование в LongTensor [[4]]
 
-        outputs = net(imgs)
-        probs = torch.softmax(outputs, dim=1)
-        masks_pred = torch.argmax(probs, dim=1)
-
-        loss = criterion(outputs, true_masks)
+        with autocast():  # Включение автоматического смешанного обучения [[1]]
+            outputs = net(imgs)
+            probs = torch.softmax(outputs, dim=1)
+            masks_pred = torch.argmax(probs, dim=1)
+            loss = criterion(outputs, true_masks)
+        
         epoch_loss += loss.item()
-
         print(f'epoch = {epoch+1:d}, iteration = {i:d}/{len(train_loader):d}, loss = {loss.item():.5f}')
-        # save to summary
-        if i%100==0:
-            writer.add_scalar('train_loss_iter', 
-                                  loss.item(), 
-                                  i + len(train_loader) * epoch)
-            writer.add_figure('predictions vs. actuals',   
-                                  plot_net_predictions(imgs, true_masks, masks_pred, batch_size),    
-                                  global_step = i + len(train_loader) * epoch)
+
+        if i % 100 == 0:
+            writer.add_scalar('train_loss_iter', loss.item(), i + len(train_loader) * epoch)
+            writer.add_figure('predictions vs. actuals', plot_net_predictions(imgs, true_masks, masks_pred, batch_size), global_step=i + len(train_loader) * epoch)
 
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        # Использование GradScaler для масштабирования градиентов [[9]]
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
         
     print(f'Epoch finished ! Loss: {epoch_loss/i:.2f}, lr:{scheduler.get_lr()}')
 
-        
-def validate_epoch(epoch,train_loader,val_loader,device):
-    
+
+def validate_epoch(epoch, train_loader, val_loader, device):
     class_iou, mean_iou = eval_net_loader(net, val_loader, 3, device)
     print('Class IoU:', ' '.join(f'{x:.3f}' for x in class_iou), f'  |  Mean IoU: {mean_iou:.3f}') 
-    # save to summary
     writer.add_scalar('mean_iou', mean_iou, len(train_loader) * (epoch+1))
-    
     return mean_iou
- 
+
 
 def train_net(train_loader, val_loader, net, device, epochs=5, batch_size=1, lr=0.1, save_cp=True):
-    
-#     params = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 4}
-#     train_loader, val_loader =  make_dataloaders(dir_data, val_ratio, params)
-
     print(f'''
     Starting training:
         Epochs: {epochs}
@@ -75,35 +65,31 @@ def train_net(train_loader, val_loader, net, device, epochs=5, batch_size=1, lr=
         Device: {str(device)}
     ''')
           
-    optimizer = optim.SGD(net. parameters(),lr=lr, momentum=0.9, weight_decay=0.0005)
-    # multiply learning rate by 0.1 after 30% of epochs
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=int(0.3*epochs), gamma=0.1)
-    
     criterion = nn.CrossEntropyLoss()
-    # weighted cross entropy loss
-#     criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.3, 8.2, 1.0]).cuda()) 
     
     best_precision = 0
+    scaler = GradScaler()  # Инициализация GradScaler [[9]]
+    
     for epoch in range(epochs):
-          
         print('Starting epoch {}/{}.'.format(epoch + 1, epochs))
-        train_epoch(epoch,train_loader,criterion,optimizer,batch_size,scheduler)
-        precision = validate_epoch(epoch,train_loader,val_loader,device)
+        train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler)
+        precision = validate_epoch(epoch, train_loader, val_loader, device)
         scheduler.step()
 
-        if save_cp and (precision>best_precision):
+        if save_cp and (precision > best_precision):
             state_dict = net.state_dict()
-            if device=="cuda":
+            if device == "cuda":
                 state_dict = net.module.state_dict()
-            torch.save(state_dict, dir_checkpoint+f'CP{epoch + 1}.pth')
+            torch.save(state_dict, dir_checkpoint + f'CP{epoch + 1}.pth')
             print('Checkpoint {} saved !'.format(epoch + 1))
             best_precision = precision
     
     writer.close()
 
-    
+
 def get_args():
-          
     parser = OptionParser()
     parser.add_option('-e', '--epochs', dest='epochs', default=5, type='int',
                       help='number of epochs')
@@ -121,8 +107,7 @@ def get_args():
 
 
 if __name__ == '__main__':
-    
-    device = ("cuda" if torch.cuda.is_available() else "cpu" )
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     args = get_args()
     
     dir_data = f'{args.folder}'
@@ -133,8 +118,8 @@ if __name__ == '__main__':
     make_checkpoint_dir(dir_checkpoint)
     writer = SummaryWriter(dir_summary)
     
-    val_ratio=0.1
-    train_loader, val_loader =  make_dataloaders(dir_data, val_ratio, params)
+    val_ratio = 0.1
+    train_loader, val_loader = make_dataloaders(dir_data, val_ratio, params)
     
     net = UNet(n_channels=3, n_classes=5)
     net.to(device)
@@ -143,7 +128,6 @@ if __name__ == '__main__':
         net.load_state_dict(torch.load(args.load))
         print('Model loaded from {}'.format(args.load))
     
-    # train model in parallel on multiple-GPUs
     if torch.cuda.device_count() > 1:
         print("Model training on", torch.cuda.device_count(), "GPUs")
         net = nn.DataParallel(net) 
