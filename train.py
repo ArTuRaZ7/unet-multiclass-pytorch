f33ac793e6a477a8c3b4dea851c2d0448de05abe
ArTuRaZ7 <49605245+ArTuRaZ7@users.noreply.github.com>
2025-06-06 11:52:27 +0500

    Update train.py

--- train.py
+++ train.py
@@ -13,10 +13,10 @@ from model import UNet, make_dataloaders, eval_net_loader, make_checkpoint_dir
 from lib import plot_net_predictions
 
 
-def train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler):
+def train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler, scaler):
     net.train()
     epoch_loss = 0
-    
+
     for i, sample_batch in enumerate(train_loader):
         imgs = sample_batch['image']
         true_masks = sample_batch['mask']
@@ -29,26 +29,28 @@ def train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler
             probs = torch.softmax(outputs, dim=1)
             masks_pred = torch.argmax(probs, dim=1)
             loss = criterion(outputs, true_masks)
-        
+
         epoch_loss += loss.item()
         print(f'epoch = {epoch+1:d}, iteration = {i:d}/{len(train_loader):d}, loss = {loss.item():.5f}')
 
         if i % 100 == 0:
             writer.add_scalar('train_loss_iter', loss.item(), i + len(train_loader) * epoch)
-            writer.add_figure('predictions vs. actuals', plot_net_predictions(imgs, true_masks, masks_pred, batch_size), global_step=i + len(train_loader) * epoch)
+            writer.add_figure('predictions vs. actuals',
+                              plot_net_predictions(imgs, true_masks, masks_pred, batch_size),
+                              global_step=i + len(train_loader) * epoch)
 
         optimizer.zero_grad()
         # Использование GradScaler для масштабирования градиентов [[9]]
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
-        
+
     print(f'Epoch finished ! Loss: {epoch_loss/i:.2f}, lr:{scheduler.get_lr()}')
 
 
 def validate_epoch(epoch, train_loader, val_loader, device):
     class_iou, mean_iou = eval_net_loader(net, val_loader, 3, device)
-    print('Class IoU:', ' '.join(f'{x:.3f}' for x in class_iou), f'  |  Mean IoU: {mean_iou:.3f}') 
+    print('Class IoU:', ' '.join(f'{x:.3f}' for x in class_iou), f'  |  Mean IoU: {mean_iou:.3f}')
     writer.add_scalar('mean_iou', mean_iou, len(train_loader) * (epoch+1))
     return mean_iou
 
@@ -64,17 +66,17 @@ def train_net(train_loader, val_loader, net, device, epochs=5, batch_size=1, lr=
         Checkpoints: {str(save_cp)}
         Device: {str(device)}
     ''')
-          
+
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=int(0.3*epochs), gamma=0.1)
     criterion = nn.CrossEntropyLoss()
-    
+
     best_precision = 0
     scaler = GradScaler()  # Инициализация GradScaler [[9]]
-    
+
     for epoch in range(epochs):
         print('Starting epoch {}/{}.'.format(epoch + 1, epochs))
-        train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler)
+        train_epoch(epoch, train_loader, criterion, optimizer, batch_size, scheduler, scaler)  # Передаем scaler
         precision = validate_epoch(epoch, train_loader, val_loader, device)
         scheduler.step()
 
@@ -85,7 +87,7 @@ def train_net(train_loader, val_loader, net, device, epochs=5, batch_size=1, lr=
             torch.save(state_dict, dir_checkpoint + f'CP{epoch + 1}.pth')
             print('Checkpoint {} saved !'.format(epoch + 1))
             best_precision = precision
-    
+
     writer.close()
 
 
@@ -99,7 +101,7 @@ def get_args():
                       type='float', help='learning rate')
     parser.add_option('-c', '--load', dest='load',
                       default=False, help='load file model')
-    parser.add_option('-f', '--folder', dest='folder', 
+    parser.add_option('-f', '--folder', dest='folder',
                       default='', help='folder name')
 
     (options, args) = parser.parse_args()
@@ -109,7 +111,7 @@ def get_args():
 if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
     args = get_args()
-    
+
     dir_data = f'{args.folder}'
     dir_checkpoint = f'./checkpoints/{args.folder}_b{args.batchsize}/'
     dir_summary = f'./runs/{args.folder}_b{args.batchsize}'
@@ -117,24 +119,24 @@ if __name__ == '__main__':
 
     make_checkpoint_dir(dir_checkpoint)
     writer = SummaryWriter(dir_summary)
-    
+
     val_ratio = 0.1
     train_loader, val_loader = make_dataloaders(dir_data, val_ratio, params)
-    
+
     net = UNet(n_channels=3, n_classes=5)
     net.to(device)
 
     if args.load:
         net.load_state_dict(torch.load(args.load))
         print('Model loaded from {}'.format(args.load))
-    
+
     if torch.cuda.device_count() > 1:
         print("Model training on", torch.cuda.device_count(), "GPUs")
-        net = nn.DataParallel(net) 
+        net = nn.DataParallel(net)
 
     try:
         train_net(train_loader, val_loader, net, device, epochs=args.epochs, batch_size=args.batchsize, lr=args.lr)
-        
+
     except KeyboardInterrupt:
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
         print('Saved interrupt')
